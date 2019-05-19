05ba7bd228bab5d4326757943760d5d07a15a2b8
Danielle <daniellefunnygraan@gmail.com>
2019-05-19 17:14:52 +0200

    add projectdocument

--- populateDB.sql
+++ populateDB.sql
@@ -64,6 +64,23 @@ INSERT INTO `dbredb`.`Document` (`Type_ID`, `Name`, `DocData`) VALUES (9, 'A Wea
 INSERT INTO `dbredb`.`Document` (`Type_ID`, `Name`, `DocData`) VALUES (10, 'Requirement Types', "");
 INSERT INTO `dbredb`.`Document` (`Type_ID`, `Name`, `DocData`) VALUES (11, 'Doctor Patient Relationships', "");
 
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('1', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('2', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('3', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('4', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('5', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('6', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('7', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('8', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('9', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('10', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('11', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('12', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('13', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('14', '1');
+INSERT INTO `dbredb`.`projectdocument` (`Document_ID`, `Project_ID`) VALUES ('15', '1');
+
+
 
 INSERT INTO `dbredb`.`user` (`Name`, `Description`) VALUES ('Gillian Moyers', 'Night staff Technician');
 INSERT INTO `dbredb`.`user` (`Name`, `Description`) VALUES ('Phil Ben Zaik', 'Temp Nurse ');
